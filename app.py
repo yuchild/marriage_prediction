83055694c62160dd51059c6a24dbe2147d88bd18
david <yuchild@gmail.com>
2020-09-26 17:11:14 -0700

    Updated

--- app.py
+++ app.py
@@ -13,7 +13,18 @@ def home():
 def predcit():
     import joblib
     model = joblib.load('marriage_age_prediction_model.ml')
-    age = model.predict([[1, 2, 5, 6, 5, 175]])
+    age = model.predict([[int(request.args['gender'])
+                          , int(request.args['caste'])
+                          , int(request.args['religion'])
+                          , int(request.args['mother_tongue'])
+                          , int(request.args['country'])
+                          , int(request.args['height_cms'])
+                         ]])
+    # in url: 
+    # http://127.0.0.1:5000/predict? \ 
+    # gender=1&caste=2&religion=2&mother_tongue=5 \ 
+    # &country=5&height_cms=175
+    
     return str(age)
 
 app.run(debug=True)
